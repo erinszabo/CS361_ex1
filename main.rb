79aead06eba1940b7cd73571fa3a51b80786b765
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-09-26 18:48:59 -0700

    made game plan

--- main.rb
+++ main.rb
@@ -1,1 +1,27 @@
-#main
+# Each Player has a name and score, 
+# and should always be initialized with a name and score.
+
+class Player
+    def initialize(name)
+        @name = name # Player 1, Player 2, etc.
+        #score
+    end
+
+    # ToString type of thing here
+    # to say: "Ready {name}! Score: {score}"
+end
+
+# func to make a random int score between 10 and 300
+
+
+### Main: ### 
+
+# create players array 
+
+# use iterater to... (possibly all in one line)
+#   get a random number to set as score 
+#   create player object and apd to the array
+# 50 times
+
+# print player string for each player
+
